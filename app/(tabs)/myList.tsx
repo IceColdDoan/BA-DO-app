e4181d773f062cfdbfa63c9e53f3e9bf26c1c154
Benson Doan <doanbenson@gmail.com>
2024-02-24 21:23:57 -0800

    uhhh we restarted LOL

---
+++ app/(tabs)/myList.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const myList = () => {
+  return (
+    <View>
+      <Text>myList</Text>
+    </View>
+  )
+}
+
+export default myList;
